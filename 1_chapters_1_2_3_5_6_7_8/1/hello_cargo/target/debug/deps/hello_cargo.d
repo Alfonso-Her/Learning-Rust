8f62fdead038149fbc0967081ac032ae65e0438f
Alfonso-Her <47159592+Alfonso-Her@users.noreply.github.com>
2025-05-25 21:50:40 +0200

    Chapter 1 finished

---
+++ 1_chapters_1_2_3_5_6_7_8/1/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Usuario\Desktop\GitHub\Learning-Rust\1_chapters_1_2_3_5_6_7_8\1\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\Usuario\Desktop\GitHub\Learning-Rust\1_chapters_1_2_3_5_6_7_8\1\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ 1_chapters_1_2_3_5_6_7_8/1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Usuario\Desktop\GitHub\Learning-Rust\1_chapters_1_2_3_5_6_7_8\1\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Usuario\Desktop\GitHub\Learning-Rust\1_chapters_1_2_3_5_6_7_8\1\hello_cargo\src\main.rs
